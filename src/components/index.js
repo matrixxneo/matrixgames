405c964d1d95aef82c233786286a207bb45af8ca
miguelalejandrodev <163203781+miguelalejandrodev@users.noreply.github.com>
2024-09-01 18:09:43 -0400

    update games icon navbar

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,33 @@
-const textWelcome = document.getElementById('textWelcome');
+// const textWelcome = document.getElementById('textWelcome');
+const textName = document.getElementById("textName");
+const textSecondary = document.getElementById("textSecondary");
+let init = 1;
+let init2 = 0;
+let velocity = 50;
+console.log(textName);
+console.log(textSecondary);
+let wordsName = "!🤓Hola, soy Matrix!";
+let wordsSecondary =
+  "Bienvenido a la Web donde puedes encontrar,filtrar o ver lascategorias de sus juegosfavoritos por plataforma, si necesitas de alguna información o juego que no se encuentre dentro de la base de datos, si es de su agrado escriba o llame al tel: #53405773 y será atendido rapidamente.";
+console.log(wordsSecondary.length);
 
-let words = 'Bienvenido a la Web donde puedes encontrar sus juegos favoritos'
-words.splice(0,2)
-textWelcome.innerHTML = words
+const mecanografiar = (textNam, textSecond) => {
+  init++;
+  let result = textNam.slice(0, init);
+  textName.innerText = result;
+  console.log(textNam.length);
+
+  if (result.length === 20) {
+    init2++;
+    velocity = 30;
+    let result2 = textSecond.slice(0, init2);
+    textSecondary.innerText = result2;
+    if (result2.length === 327) {
+      return;
+    }
+  }
+
+  setTimeout(() => mecanografiar(textNam, textSecond), velocity);
+};
+
+mecanografiar(wordsName, wordsSecondary);
