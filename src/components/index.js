fb74826879fe67d24e5781c805500456b4260884
miguelalejandrodev <163203781+miguelalejandrodev@users.noreply.github.com>
2024-09-11 04:59:28 -0400

    add hen 9.00

--- src/components/index.js
+++ src/components/index.js
@@ -4,18 +4,17 @@ const textSecondary = document.getElementById("textSecondary");
 let init = 1;
 let init2 = 0;
 let velocity = 50;
-console.log(textName);
-console.log(textSecondary);
+
 let wordsName = "!🤓Hola, soy Matrix!";
 let wordsSecondary =
-  "Bienvenido a la Web donde puedes encontrar,filtrar o ver lascategorias de sus juegosfavoritos por plataforma, si necesitas de alguna información o juego que no se encuentre dentro de la base de datos, si es de su agrado escriba o llame al tel: #53405773 y será atendido rapidamente.";
-console.log(wordsSecondary.length);
+  "Bienvenido a la Web donde puedes encontrar,filtrar o ver las categorias de sus juegos favoritos por plataforma, si necesitas de alguna información o juego que no se encuentre dentro de la base de datos, si es de su agrado escriba o llame al tel: #53405773 y será atendido rapidamente.";
+
 
 const mecanografiar = (textNam, textSecond) => {
   init++;
   let result = textNam.slice(0, init);
   textName.innerText = result;
-  console.log(textNam.length);
+
 
   if (result.length === 20) {
     init2++;
